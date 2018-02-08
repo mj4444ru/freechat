bea21c2e6085c7a3fe9c0c352f6ca1a9a1ae15da
mj4444ru <mj4444@mail.ru>
2018-02-09 02:44:20 +0500

    db.simple.php

---
+++ www/config/db.simple.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'class' => yii\db\Connection::class,
+    'dsn' => 'mysql:host=mysql;dbname=fc',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    'tablePrefix' => 'fc_',
+    'enableSchemaCache' => true,
+];
