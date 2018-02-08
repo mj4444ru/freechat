18abebc30a577713b9c8226868af763699ea9171
mj4444ru <mj4444@mail.ru>
2018-02-09 00:26:07 +0500

    github-hook.php

--- www/web/dev/github-hook.php
+++ www/web/dev/github-hook.php
@@ -7,8 +7,20 @@ file_put_contents($logfile, $logstr, FILE_APPEND | LOCK_EX);
 $path = realpath(__DIR__ . '/../../../');
 $logstr = `cd {$path}; git pull` . "\n";
 file_put_contents($logfile, $logstr, FILE_APPEND | LOCK_EX);
-$path = realpath(__DIR__ . '/../../../');
-$logstr = `cd {$path}; composer install` . "\n";
+$logstr = `cd {$path}/www; composer install` . "\n";
+file_put_contents($logfile, $logstr, FILE_APPEND | LOCK_EX);
+$logstr = `cd {$path}/www; {$path}/www/yii migrate --interactive=0` . "\n";
+file_put_contents($logfile, $logstr, FILE_APPEND | LOCK_EX);
+$logstr = `cd {$path}/www; {$path}/www/yii cache/flush` . "\n";
+file_put_contents($logfile, $logstr, FILE_APPEND | LOCK_EX);
+$logstr = `cd {$path}/www; {$path}/www/yii cache/flush-all` . "\n";
 file_put_contents($logfile, $logstr, FILE_APPEND | LOCK_EX);
 $logstr = "\nEnd auto-deploy at " . date("r") . "\n\n================================================================================\n\n";
 file_put_contents($logfile, $logstr, FILE_APPEND | LOCK_EX);
+if (function_exists('apc_clear_cache')) {
+    apc_clear_cache();
+    apc_clear_cache('user');
+}
+if (function_exists('apcu_clear_cache')) {
+    apcu_clear_cache();
+}
